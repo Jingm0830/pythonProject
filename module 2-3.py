cd6ebcf5ef1aa74a05dff585142064473d8c20a2
8.25 Amir <jingm@metropolia.fi>
2023-09-04 01:46:32 +0300

    2-3

---
+++ module 2-3.py
@@ -0,0 +1,8 @@
+length_str = input("length, L: ")
+width_str = input("width, W: ")
+length = float(length_str)
+width = float(width_str)
+perimeter = (length*2)+(width*2)
+area = length*width
+print("The perimeter of the rectangle is: " + str(perimeter))
+print("The area of the rectangle is: " + str(area))
