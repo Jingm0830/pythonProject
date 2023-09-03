4bb0f458311cfb2d80607206c63ae1b58c05206f
8.25 Amir <jingm@metropolia.fi>
2023-09-04 02:03:01 +0300

    2-4

---
+++ module 2-4.py
@@ -0,0 +1,12 @@
+number1_str = input("NO.1: ")
+number2_str = input("NO.2: ")
+number3_str = input("NO.3: ")
+number1 = int(number1_str)
+number2 = int(number2_str)
+number3 = int(number3_str)
+Sum = number1 + number2 + number3
+Product = number1 * number2 * number3
+Avarage = (number1 + number2 + number3)/3
+print("The Sum of the numbers is: " + str(Sum))
+print("The Product of the numbers is: "+ str(Product))
+print("The Avarage of the numbers is: "+ str(Avarage))
