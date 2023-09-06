f6a950bb80ddab887d048fadb63a6dca7175c3e9
8.25 Amir <jingm@metropolia.fi>
2023-09-06 23:22:51 +0300

    3-1

--- module 3-1.py
+++ module 3-1.py
@@ -5,3 +5,5 @@ if length < size_limit:
     print("not fulfill the size limit!!!")
     print("size limit:>=42cm")
     print("release the fish back into the lake, please!")
+elif length >= 42:
+    print("Hyvää!!!")
