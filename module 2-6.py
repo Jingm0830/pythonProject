81d6be860fe07dc6923f900c6dd77665c3d6de4d
8.25 Amir <jingm@metropolia.fi>
2023-09-06 22:22:44 +0300

    2-6

---
+++ module 2-6.py
@@ -0,0 +1,10 @@
+import random
+x1 = str(random.randint(0, 9))
+x2 = str(random.randint(0, 9))
+x3 = str(random.randint(0, 9))
+print("PIN1: " +(x1+x2+x3))
+y1 = str(random.randint(1, 6))
+y2 = str(random.randint(1, 6))
+y3 = str(random.randint(1, 6))
+y4 = str(random.randint(1, 6))
+print("PIN2: " +(y1+y2+y3+y4))
