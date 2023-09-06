f77e803535083799e21b8b0b328e9fce52a7af9f
8.25 Amir <jingm@metropolia.fi>
2023-09-06 09:24:23 +0300

    3-1

---
+++ module 3-1.py
@@ -0,0 +1,7 @@
+length = input("Tell me the length of the zander in centimeters: ")
+size_limit = 42
+length = float(length)
+if length < size_limit:
+    print("not fulfill the size limit!!!")
+    print("size limit:>=42cm")
+    print("release the fish back into the lake, please!")
