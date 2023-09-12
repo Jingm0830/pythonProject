791f53e4b4f63c86cac1668c04e7607c56b122fb
8.25 Amir <jingm@metropolia.fi>
2023-09-12 22:16:10 +0300

    module 4-2

--- module 4-2.py
+++ module 4-2.py
@@ -5,6 +5,7 @@ while inches >= 0:
     if inches >= 0:
       length_cm = round(inches * 2.54,2)
       print(f"{inches}inches = {length_cm}cm")
+
     elif inches < 0:
         print("program ends.")
 
