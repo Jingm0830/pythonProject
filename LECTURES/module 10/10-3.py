375433f318587ed4505f37001880796c9d7fd648
8.25 Amir <jingm@metropolia.fi>
2023-11-21 23:35:58 +0200

    10-3

--- LECTURES/module 10/10-3.py
+++ LECTURES/module 10/10-3.py
@@ -1,7 +1,7 @@
 """
 Extend the program again by adding a method fire_alarm that does not receive any parameters
 and moves all elevators to the bottom floor.
-Continue the main program by causing a fire alarm in your building.
+Continue the main program b y causing a fire alarm in your building.
 """
 class Elevator:
     def __init__(self, elevator_number, button_floor=1):
